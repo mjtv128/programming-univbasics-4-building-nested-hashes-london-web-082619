e7735d60f25dd4bde95136cd0b644d6666cd7568
jocular-source-7570 <mjtvan@gmail.com>
2019-08-09 12:10:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,10 +11,10 @@ end
 def monopoly_with_third_tier
    monopoly = { railroads: 
    { pieces: 4, 
-   rent_in_dollars: { one_piece_owned: {}, 
-     two_pieces_owned: {}, 
-     three_pieces_owned: {}, 
-     four_pieces_owned: {}
+   rent_in_dollars: { one_piece_owned: 25, 
+     two_pieces_owned: 50,
+     three_pieces_owned: 100, 
+     four_pieces_owned: 200
    }, 
    names: { reading_railroad: {}, 
      pennsylvania_railroad: {},
